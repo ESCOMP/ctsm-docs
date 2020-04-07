d84408881a347d8adb9e9819b5ff1b8a5a92b5bb
Bill Sacks <sacks@ucar.edu>
2020-04-07 14:44:32 -0600

    Update release docs version
    From 0380be44 - use branch name rather than specific tag for release

--- versions/release-clm5.0/html/_static/documentation_options.js
+++ versions/release-clm5.0/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'release-clm5.0.32',
+    VERSION: 'release-clm5.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
