cd4b0bd970c6a47eeee9d7013fde0797a15b16be
Bill Sacks <sacks@ucar.edu>
2020-04-07 11:53:38 -0600

    Rebuild documentation from d56983de - changes in conf.py

--- versions/master/html/_static/documentation_options.js
+++ versions/master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master',
+    VERSION: 'CTSM master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
